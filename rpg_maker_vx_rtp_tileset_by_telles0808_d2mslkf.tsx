849cfc54ecb5feea85c987c36543c96709ee02b5
stchew0610 <93031238+stchew0610@users.noreply.github.com>
2021-10-23 17:28:39 +0800

    Add files via upload

---
+++ rpg_maker_vx_rtp_tileset_by_telles0808_d2mslkf.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="rpg_maker_vx_rtp_tileset_by_telles0808_d2mslkf" tilewidth="32" tileheight="32" tilecount="2016" columns="32">
+ <image source="../../../Downloads/rpg_maker_vx_rtp_tileset_by_telles0808_d2mslkf.png" width="1024" height="2016"/>
+</tileset>
